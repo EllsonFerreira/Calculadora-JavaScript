339a7474bf0318cd33d91cd7df4ab28f81ec54d7
EllsonFerreira <ellsonpjlvab@gmail.com>
2022-05-25 21:23:35 -0300

    incluindo arquivos da calculadora em Js

---
+++ tiposDeFuncao.js
@@ -0,0 +1,24 @@
+// tipos de função
+
+/*  // funções declarativas
+function funcao() {
+    console.log('Sou uma mensagem de uma função declarativa.')
+}
+
+funcao()*/
+
+// expressão de função
+
+/*var funcao = function() {
+    console.log('Sou uma mensagem de uma função de expressão.')
+}
+
+funcao()*/
+
+// Arrow function (função com seta)
+
+/*var funcao = () => {
+    console.log('Sou uma mensagem de uma Arrow Function.')
+}
+
+funcao()*/
